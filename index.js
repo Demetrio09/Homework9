1fd314b7cbf3628c22079e920a12b769c4932f2e
Demetrio <demetrioalmeida@gmail.com>
2020-05-20 01:11:34 -0400

    Homework9

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
+
+genereteReadme();
+
+async function genereteReadme() {
+    try {
+        const { userName } = await inquirer.prompt({
+            message: "What is your GitHub username?",
+            name: "userName"
+        });
+        const { email } = await inquirer.prompt({
+            message: "What is your email?",
+            name: "email"
+        });
+        const { url } = await inquirer.prompt({
+            message: "the URL to your project?",
+            name: "url"
+        });
+        const { projectName } = await inquirer.prompt({
+            message: "What is your project's name?",
+            name: "projectName"
+        });
+        const { description } = await inquirer.prompt({
+            message: "Please write a short description of your project",
+            name: "description"
+        });
+        const { license } = await inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'))
+        inquirer.prompt({
+            type: "autocomplete",
+            message: "What kind of license should your project have?",
+            name: "license"
+        });
+        const { command } = await inquirer.prompt({
+            type: "autocomplete",
+            message: "What command should be run to install dependencies?",
+            name: "command"
+        });
+        console.log(`what you have typed ${userName}, ${email}, ${url}, ${projectName}, ${description}, ${license}, ${command}!`);
+
+    } catch (err) {
+        console.log(err);
+    }
+}
